2537cdf1a774cc284e43d7fd7cb4763e33b1f0f6
Nick Trinh <aen_tn@hotmail.com>
2022-02-01 14:14:34 -0500

    Add ingredient list to card

--- main.js
+++ main.js
@@ -2,21 +2,43 @@ const app = Vue.createApp({
   data() {
     return {
       mainIngredient: "",
+      diet: "",
+      cuisineType: "",
+      mealType: "",
+      dishType: "",
       recipes: null,
       errors: null,
     };
   },
   methods: {
     searchRecipe(e) {
-      fetch(
+      string =
         "https://api.edamam.com/api/recipes/v2?type=public&q=" +
-          this.mainIngredient +
-          "&app_id=26d89fcd&app_key=28d64d9439085ffc46d2b5ac16b31fc4",
-        {
-          method: "GET",
-          headers: {},
-        }
-      )
+        encodeURI(this.mainIngredient) +
+        "&app_id=26d89fcd&app_key=28d64d9439085ffc46d2b5ac16b31fc4";
+
+      if (this.diet) {
+        string += "&diet=" + encoreURI(this.diet);
+      }
+
+      if (this.cuisineType) {
+        string += "&cuisineType=" + encodeURI(this.cuisineType);
+      }
+
+      if (this.mealType) {
+        string += "&mealType=" + encodeURI(this.mealType);
+      }
+
+      if (this.dishType) {
+        string += "&dishType=" + encodeURI(this.dishType);
+      }
+
+      string += "&random=true";
+
+      fetch(string, {
+        method: "GET",
+        headers: {},
+      })
         .then((response) => response.json())
         .then((response) => {
           // if (response.ok) {
