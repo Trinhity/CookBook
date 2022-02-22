1020949531d81a1967fbf61f8789780967af4bb2
Nick Trinh <aen_tn@hotmail.com>
2022-02-22 00:13:35 -0500

    Set up backend file structure

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("bodyParser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -8,6 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = require("./routes/api/posts");
+
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server started on port ${port}"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
