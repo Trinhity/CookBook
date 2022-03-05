1800daf721396dbff2d1d40260c06efc9f59623e
Nick Trinh <aen_tn@hotmail.com>
2022-03-04 22:11:27 -0500

    Add basic layout and side nav bar

---
+++ client/src/views/BookmarkedView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Bookmarked Recipes</h1>
+</template>
--- frontend/components/VueBasicAlert.js
+++
@@ -1,7 +0,0 @@
-import VueBasicAlert from "vue-basic-alert";
-
-export default defineComponent({
-  components: {
-    VueBasicAlert,
-  },
-});
