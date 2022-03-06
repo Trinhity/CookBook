530c756c05a0976dd86fcb065c3135c4d2c96d80
Nick Trinh <aen_tn@hotmail.com>
2022-03-05 23:25:24 -0500

    Add functionality for search bar

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,28 +1,24 @@
 <template>
   <v-container>
     <h1>Home page</h1>
-    <v-form>      
+    <v-form>  
       <v-row>
         <v-col cols="12" sm="6" md="3">
+          
           <v-text-field
+            v-model="mainIngredient"
+          
             label="What are you craving?"
             placeholder="Key Ingredient"
+            outlined
+            rounded
+            clearable
+            append-outer-icon="mdi-magnify"
+            @click:append-outer="searchRecipe"
           ></v-text-field>
-          <v-btn
-              icon
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
+          
         </v-col>
-        <v-col cols="12" sm="6" md="3">
-          <v-btn
-              icon
-            >
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-        </v-col>
-      </v-row>        
-    
+      </v-row>           
     </v-form>
   </v-container>
 </template>
@@ -32,12 +28,69 @@
 
   export default {
     name: 'Home',
-    data() {
-      return {}
-    },
+    data: () => ({
+      mainIngredient: ""
+      
+    }),
 
     components: {
-      
+     
+    },
+
+    methods: {
+      searchRecipe(e) {
+        string =
+          "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+          encodeURI(this.mainIngredient) +
+          "&app_id=26d89fcd&app_key=28d64d9439085ffc46d2b5ac16b31fc4";
+
+        if (this.diet != "") {
+          string += "&diet=" + encodeURI(this.diet);
+        }
+
+        if (this.health != "") {
+          string += "&health=" + encodeURI(this.health);
+        }
+
+        if (this.cuisineType != "") {
+          string += "&cuisineType=" + encodeURI(this.cuisineType);
+        }
+
+        if (this.mealType != "") {
+          string += "&mealType=" + encodeURI(this.mealType);
+        }
+
+        if (this.dishType != "") {
+          string += "&dishType=" + encodeURI(this.dishType);
+        }
+
+        string += "&random=true";
+
+        fetch(string, {
+          method: "GET",
+          headers: {},
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.hits.length > 0) {
+              this.recipes = response.hits;
+            } else {
+              this.errors = "Maybe you should invent this dish";
+              alert(
+                "Server returned " + response.status + " : " + response.statusText
+              );
+            }
+
+            return {
+              recipes: this.recipes,
+              errors: this.errors,
+            };
+          });
+
+        e.preventDefault();
+        // this.recipes = null;
+        // this.errors = null;
+      },
     },
   }
 </script>
