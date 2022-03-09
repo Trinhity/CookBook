e3351adb11112fb4e13f380ee83356994ba09293
Nick Trinh <aen_tn@hotmail.com>
2022-03-08 21:08:22 -0500

    Adjust size for each card for consistency

--- client/src/views/BookmarkedView.vue
+++ client/src/views/BookmarkedView.vue
@@ -1,15 +1,16 @@
 <template>    
   <v-container>
-    
     <v-row no-gutters>
       <v-col
-        sm="4"
-        class="pa-3"
+        sm="3"
+        class="pa-3 d-flex flex-row"
         v-for="recipe in recipes"
         :key="recipe._id"
         cols="4"
       >                    
-        <v-card>
+        <v-card
+          class="d-flex flex-column"
+        >
           <v-hover v-slot="{ hover }">  
             <v-img height="250" :src="`${recipe.recipe.image}`">
               <v-fade-transition>
@@ -24,12 +25,13 @@
             </v-img>
           </v-hover> 
 
-          <v-card-title class="indigo white--text"
-            v-if="recipe.recipe.label.length > 20"
+          <v-card-title  
+            class="brown white--text"
+            v-if="recipe.recipe.label.length > 100"
           >
-            {{ recipe.recipe.label.substring(0, 20)+"..." }}
+            {{ recipe.recipe.label.substring(0, 100)+"..." }}
           </v-card-title>
-          <v-card-title class="indigo white--text"
+          <v-card-title class="brown white--text"
             v-else
           >
             {{ recipe.recipe.label }}
@@ -39,35 +41,50 @@
 
           <v-row no-gutters class="pa-4" >
             <v-col>
-              <v-btn small outlined color="indigo">
+              <v-btn small outlined color="#9C6644">
                 {{ recipe.recipe.dishType[0] }}
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn small outlined color="indigo">
+              <v-btn small outlined color="#9C6644">
                 {{ recipe.recipe.mealType[0] }}
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn small outlined color="indigo">
+              <v-btn small outlined color="#9C6644">
                 {{ recipe.recipe.cuisineType[0] }}
               </v-btn>
             </v-col>
           </v-row>
 
           <v-divider></v-divider>
-          
-          <v-expansion-panels flat popout>
-            <v-expansion-panel>
-              <v-expansion-panel-header ripple>Ingredients</v-expansion-panel-header>
-              <v-expansion-panel-content
+
+          <v-menu
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="#B08968"
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                Ingredients
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
                 v-for="ingredient in recipe.recipe.ingredientLines"
                 :key="ingredient"
               >
-                {{ ingredient }}
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
+                <v-list-item-title>{{ ingredient }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
         </v-card>    
       </v-col>
     </v-row> 
@@ -84,6 +101,7 @@
       return {
         recipes: [],
         overlay: false,
+
         
       }
     },
